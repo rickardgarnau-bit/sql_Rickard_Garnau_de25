0d063b7609d79d68556ba54f1d89412f9c13ad38
Rickard Garnau <rickard.garnau89@gmail.com>
2025-11-14 14:03:22 +0100

    more

--- SQL_code_alongs/bike_data/sql/query_bike.sql
+++ SQL_code_alongs/bike_data/sql/query_bike.sql
@@ -1,5 +1,5 @@
 /*=============
- Query teh data
+ Query the data
  ===============*/
 -- overview of data
 DESC;
@@ -53,14 +53,88 @@ VALUES
 
 -- sort the rows by order_status
 -- ORDER BY ... ASC/DESC
-/* EXAMPLE
- SELECT 
- j.order_id,
- j.order_status,
- s.order_status_description
- FROM staging.joined_tables j --alias 'j'
- -- join status description to this table
- JOIN staging.status s ON j.order_status = s.order_status
- --ORDER BY j.order_status ASC;
- ORDER BY j.order_status DESC; 
- */
+SELECT
+    j.order_id,
+    j.order_status,
+    s.order_status_description
+FROM
+    staging.joined_tables j --alias 'j'
+    -- join status description to this table
+    JOIN staging.status s ON j.order_status = s.order_status --ORDER BY j.order_status ASC;
+ORDER BY
+    j.order_status DESC;
+
+/* =================
+ Investigate unique 
+ customers
+ ==================== */
+-- DISTINCT
+SELECT
+    DISTINCT order_id
+FROM
+    staging.joined_tables
+ORDER BY
+    order_id ASC;
+
+-- find unique values of customer_id
+SELECT
+    DISTINCT customer_id
+FROM
+    staging.joined_tables
+ORDER BY
+    customer_id ASC;
+
+-- find unique values of customer fullname
+SELECT
+    DISTINCT customer_first_name,
+    customer_last_name
+FROM
+    staging.joined_table
+ORDER BY
+    customer_first_name,
+    customer_last_name;
+
+/* =============
+ Introduce
+ aggregation
+ ===============*/
+-- aggregate over rows
+-- there are different ways of aggregation (max, min...)
+-- It is 'Justina Jenkins' that is the issue
+-- this can be found out by one window function
+-- below also shows WHERE clause with two conditions
+SELECT
+    customer_id,
+    customer_first_name,
+    customer_last_name,
+    customer_city
+FROM
+    staging.joined_tables
+WHERE
+    customer_first_name = 'Justina'
+    AND customer_last_name = 'Jenkins';
+
+-- what is the total revenue from all orders
+SELECT
+    ROUND(MIN(quantity * list_price)) AS total_revenue,
+    ROUND(MAX(quantity * list_price)) AS max_revenue
+FROM
+    staging.joined_tables;
+
+/* =============
+ CASE...WHEN
+ =============*/
+-- similar to 'if else' in other languages
+-- we can replace the order_status column to some descriptions
+SELECT
+    order_id,
+    product_name,
+    CASE
+        order_status
+        WHEN 1 THEN 'Pending'
+        WHEN 2 THEN 'Processing'
+        WHEN 3 THEN 'Rejected'
+        WHEN 4 THEN 'Completed'
+    END AS order_status_description
+FROM
+    staging.joined_tables;
